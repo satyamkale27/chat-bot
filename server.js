f17ebde51d81a5acd9afee6e2e9fecf204fb41bc
satyam kale <satyamkale271@gmail.com>
2024-03-22 17:50:24 +0530

    added api key

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const port = 8000;
+const PORT = 8000;
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+const LANGUAGE_MODEL_API_KEY = process.env.LANGUAGE_MODEL_API_KEY;
+const LANGUAGE_MODEL_URL = `https://generativelanguage.googleapis.com/v1beta1/models/chat-bison-001:generateMessage?key=${LANGUAGE_MODEL_API_KEY}`;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
