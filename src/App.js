e06e2ad5b2e12307afff9f9e7bfad07fb040f394
satyam kale <satyamkale271@gmail.com>
2024-03-22 16:23:47 +0530

    svg added in resources

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 const App = () => {
-  return <div className="chat-bot"></div>;
+  return (
+    <div className="chat-bot">
+      <div className="chat-header">
+        <div className="info-container">
+          <h3>Chat with</h3>
+          <h2>Gemini Bot</h2>
+        </div>
+        {/* //svg */}
+      </div>
+      <div className="feed">
+        <div className="questions bubble"></div>
+        <div className="response bubble"></div>
+      </div>
+      {/* <textarea value={} onChange={}/> */}
+      {/* <button onClick={}>⇾</button> */}
+    </div>
+  );
 };
 
 export default App;
